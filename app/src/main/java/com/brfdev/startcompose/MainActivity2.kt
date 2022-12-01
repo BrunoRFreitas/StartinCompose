39e725ef27999563e62d0e1becbd91c60c2ed9ba
Bruno Freitas <bruno.freitas@meta.com.br>
2022-11-30 21:42:28 -0300

    finalizando 2a aula

---
+++ app/src/main/java/com/brfdev/startcompose/MainActivity2.kt
@@ -0,0 +1,53 @@
+package com.brfdev.startcompose
+
+import android.graphics.drawable.shapes.OvalShape
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Text
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.geometry.toRect
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Outline
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.unit.Density
+import androidx.compose.ui.unit.LayoutDirection
+import androidx.compose.ui.unit.dp
+
+class MainActivity2 : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Column(
+                modifier = Modifier
+                    .background(Color.Green)
+                    .fillMaxHeight(0.5f)
+//                    .width(300.dp)
+//                    .requiredWidth(600.dp)
+                    .fillMaxWidth()
+//                    .padding(30.dp)
+//                    .padding(top = 30.dp)
+                    .border(10.dp, Color.Magenta)
+                    .padding(10.dp)
+                    .border(5.dp, Color.Blue)
+                    .padding(10.dp)
+                    .border(20.dp, Color.Red)
+                    .padding(20.dp)
+            ) {
+                Text(
+                    text = "Hello",
+//                    modifier = Modifier.offset(0.dp, 20.dp)
+                )
+                Spacer(modifier = Modifier.height(50.dp))
+                Text(text = "World")
+
+            }
+        }
+    }
+
+}
