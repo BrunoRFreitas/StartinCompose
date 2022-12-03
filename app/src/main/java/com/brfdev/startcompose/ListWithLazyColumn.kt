f395db1dec32a600d61fe535cbcaac03c0310e9d
Bruno Freitas <bruno.freitas@meta.com.br>
2022-12-03 19:31:31 -0300

    2nd part - class 8 - list with LazyColumn

--- app/src/main/java/com/brfdev/startcompose/ListWithLazyColumn.kt
+++ app/src/main/java/com/brfdev/startcompose/ListWithLazyColumn.kt
@@ -3,11 +3,10 @@ package com.brfdev.startcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material.Text
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
@@ -15,17 +14,16 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
-class Lists : ComponentActivity() {
+class ListWithLazyColumn: ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            val scrollState = rememberScrollState()
-            Column(
-                modifier = Modifier.verticalScroll(scrollState)
-            ) {
-                for (i in 1..50) {
+        setContent{
+            LazyColumn {
+                itemsIndexed(
+                    listOf("This", "Is", "Jetpack", "Compose")
+                ){ index, string ->
                     Text(
-                        text = "Item $i",
+                        text = "Item $string",
                         fontSize = 24.sp,
                         fontWeight = FontWeight.Bold,
                         textAlign = TextAlign.Center,
@@ -34,6 +32,17 @@ class Lists : ComponentActivity() {
                             .padding(vertical = 24.dp)
                     )
                 }
+//                items(5000){
+//                    Text(
+//                        text = "Item $it",
+//                        fontSize = 24.sp,
+//                        fontWeight = FontWeight.Bold,
+//                        textAlign = TextAlign.Center,
+//                        modifier = Modifier
+//                            .fillMaxWidth()
+//                            .padding(vertical = 24.dp)
+//                    )
+//                }
             }
         }
     }
