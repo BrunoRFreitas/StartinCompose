35c28f04ef546fadfb1444d88f5f0c2f27a7a3f0
Bruno Freitas <bruno.freitas@meta.com.br>
2022-12-07 22:01:08 -0300

    class 11 - simple animations

---
+++ app/src/main/java/com/brfdev/startcompose/Animation.kt
@@ -0,0 +1,70 @@
+package com.brfdev.startcompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColor
+import androidx.compose.animation.core.*
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+class Animation : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            var sizeState by remember {
+                mutableStateOf(200.dp)
+            }
+            val size by animateDpAsState(
+                targetValue = sizeState,
+//                keyframes {
+//                    durationMillis = 12000
+//                    sizeState at 3000 with LinearEasing
+//                    sizeState * 1.5f at 4000 with FastOutLinearInEasing
+//                    sizeState * 4f at 9000
+//                }
+//                spring(
+//                    Spring.DampingRatioHighBouncy
+//                )
+                tween(
+                    durationMillis = 3000,
+                    delayMillis = 300,
+//                    easing = LinearOutSlowInEasing
+//                    easing = FastOutSlowInEasing
+                    easing = FastOutLinearInEasing
+                )
+            )
+            val infiniteTransition = rememberInfiniteTransition()
+            val color by infiniteTransition.animateColor(
+                initialValue = Color.Red,
+                targetValue = Color.Green,
+                animationSpec = infiniteRepeatable(
+                    animation = tween(
+                        durationMillis = 2000
+                    ),
+                    repeatMode = RepeatMode.Reverse
+                )
+            )
+            Box(
+                Modifier
+                    .size(size)
+                    .background(color),
+                contentAlignment = Alignment.Center
+            ) {
+                Button(onClick = {
+                    sizeState += 50.dp
+                }) {
+                    Text(text = "Increase size")
+                }
+            }
+        }
+    }
+}
